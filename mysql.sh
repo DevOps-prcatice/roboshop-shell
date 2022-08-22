20a2c8e94ad400980393929a9873de76eaa61252
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-22 15:54:46 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- mysql.sh
+++ mysql.sh
@@ -4,8 +4,8 @@ yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 DEFAULT_PASSWD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWD}
-mysql -uroot -pRoboShop@1
+echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWD}
+uninstall plugin validate_password | mysql -uroot -P$MYSQL_PASSWD
 #> uninstall plugin validate_password;
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 cd /tmp
