330605ff1cbd1fd23155c45ab3b3738216348d12
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-24 23:41:12 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- cart.sh
+++ cart.sh
@@ -5,37 +5,37 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
 echo $?
 echo -e "\e[32m installing NodeJs \e[0m"
 yum install nodejs -y &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo -e "\e[32m Adding user roboshop \e[0m"
 useradd roboshop &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo -e "\e[32mdownloading application content\e[0m"
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
-StatusCheck
+echo $?
 cd /home/roboshop &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo Cleaning the old application content
 rm -rf cart &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo extracting application archive
 unzip /tmp/cart.zip &>>/tmp/cart.log
-StatusCheck
+echo $?
 mv cart-main cart &>>/tmp/cart.log
-StatusCheck
+echo $?
 cd cart &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo installing NodeJs Dependencies
 npm install &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo configuring Cart SystemD service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
-StatusCheck
+echo $?
 systemctl daemon-reload &>>/tmp/cart.log
-StatusCheck
+echo $?
 echo starting cart service
 systemctl start cart &>>/tmp/cart.log
-StatusCheck
+echo $?
 systemctl enable cart &>>/tmp/cart.log
-StatusCheck
+echo $?
 
 
