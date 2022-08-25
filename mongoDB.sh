99007ceb3d90f04849aa30768c71886457b21b3b
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-25 16:51:41 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- mongoDB.sh
+++ mongoDB.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/bash
 
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-cd /tmp
-unzip mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+source common.sh
+COMPONENT=mongodb
+echo Setup Yum Repo
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+StatusCheck
 
+echo Install MongoDB
+yum install -y mongodb-org &>>{LOG}
+StatusCheck
+
+echo Start MongoDB Service
+systemctl enable mongod &>>{LOG} && systemctl start mongod &>>{LOG}
+StatusCheck
+
+# Update Listen Config
+systemctl restart mongod
+
+DOWNLOAD
+
+echo Extract Schema file
+cd /tmp && unzip mongodb.zip &>>{LOG}
+StatusCheck
+
+echo Load Schema
+cd mongodb-main &>>{LOG} && mongo < catalogue.js &>>{LOG} && mongo < users.js &>>{LOG}
+StatusCheck
 
