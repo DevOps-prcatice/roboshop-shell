9ec5f3eaf23ff0e9cccaa3536c332f887c5af2a5
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-25 01:24:34 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- user.sh
+++ user.sh
@@ -1,17 +1,3 @@
-set -e
-
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/user.log
-
-yum install nodejs -y &>>/tmp/user.log
-useradd roboshop &>>/tmp/user.log
-curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>/tmp/user.log
-cd /home/roboshop &>>/tmp/user.log
-rm -rf User &>>/tmp/user.log
-unzip -o /tmp/user.zip &>>/tmp/user.log
-mv user-main user &>>/tmp/user.log
-cd /home/roboshop/user &>>/tmp/user.log
-npm install &>>/tmp/user.log
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>/tmp/user.log
-systemctl daemon-reload &>>/tmp/user.log
-systemctl start user &>>/tmp/user.log
-systemctl enable user &>>/tmp/user.log
+source common.sh
+COMPONENT=user
+NODEJS
