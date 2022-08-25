516e11bd1edbcb4804e0812cc43c36303c47853e
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-25 00:01:39 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- cart.sh
+++ cart.sh
@@ -2,39 +2,39 @@ source common.sh
 
 echo  Setting nodejs repos 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo   installing NodeJs 
 yum install nodejs -y &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo   Adding Application User
 useradd roboshop &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo  downloading application content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>/tmp/cart.log
 cd /home/roboshop &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo Cleaning the old application content
 rm -rf cart &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo extracting application archive
 unzip /tmp/cart.zip &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo installing NodeJs Dependencies
 npm install &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo configuring Cart SystemD service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-echo $?
+StatusCheck
 
 echo starting cart service
 systemctl start cart &>>/tmp/cart.log
