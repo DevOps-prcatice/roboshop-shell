485abfdb32b6b2232cbb4b9153d298fd8013ca8d
[Madhu Madduri] <[madhusudhanreddy2606@gmail.com]>
2022-08-25 19:20:26 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	frontend.sh

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,10 @@
 source common.sh
 COMPONENT=mysql
 
-if [ -z "$MYSQL_PASSWD" ] ; then
+if [ -z "$MYSQL_PASSWD" ]; then
   echo -e "\e[33m env variable MYSQL_PASSWD is missing\e[0m"
   exit 1
-  fi
+fi
 
 echo Setup Yum Repo
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>{LOG}
@@ -18,8 +18,12 @@ echo Start Mysql Service
 systemctl enable mysqld &>>{LOG} && systemctl start mysqld &>>{LOG}
 StatusCheck
 
-DEFAULT_PASSWD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWD}
+echo "show databases;" | mysql -uroot -p$MYSQL_PASSWD &>>{LOG}
+if [ $? -ne 0 ] ; then
+  echo Changing the default password
+  DEFAULT_PASSWD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWD}
+fi
 
 exit
 uninstall plugin validate_password | mysql -uroot -P$MYSQL_PASSWD
